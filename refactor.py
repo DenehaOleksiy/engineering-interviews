843d8429d2b2587b29c8ed443ba9d6300e9ca0a9
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2016-03-10 21:26:14 -0600

    Create refactor.py

---
+++ refactor.py
@@ -0,0 +1,32 @@
+# Refactoring
+#
+# The following function currently exists: please refactor this code so that it's:
+#
+# - readable
+# - testable
+# - generally easier to work with.
+#
+# You do not need to write working code, just some pseudo code in the language of your choice.
+#
+# Explain to me why you made each change and how it helps with one of the following categories:
+#
+# - Readability
+# - Testing
+# - Reliability
+
+def charge(payment_instrument_id, amount, description, processor):
+    payment_instrument = models.PaymentInstrument.query.get(payment_instrument_id)
+    if not payment_instrument:
+        raise Exceptions.NotFound(payment_instrument_id)
+    if amount >=0:
+        raise Exceptions.Exception(amount + " must be supplied")
+    if amount > 100000:
+        raise Exceptions.Exception(amount + " is too large")
+    description = description or "Default Description"
+    if description.length > 22:
+        description = description[:22]
+    authorization = processor.authorize(payment_instrument, amount, description)
+    debit = processor.capture(authorization, amount)
+    if debit.success:
+        return True
+    return False
